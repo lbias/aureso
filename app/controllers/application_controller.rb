c7a2f5c5fe6f6958e6ad189d8536d74f3e3e00f6
lbias <306508354@qq.com>
2017-08-22 21:26:13 +0800

    add api tokenauthentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_token
+
+  private
+
+  def authenticate_token
+    unless authenticate_with_http_token { |token, options| token == 'secret' }
+      render json: { error: 'Invalid Token.' }, status: 401
+    end
+  end
 end
