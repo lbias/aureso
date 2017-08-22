ac813bd7c43c5ddf9f5cfeeea45a5afed5eff854
lbias <306508354@qq.com>
2017-08-22 21:41:40 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+organization = Organization.create(name: 'Aureso',
+                                   public_name: 'Aureso',
+                                   type: 'Service',
+                                   pricing_policy: 'Fixed')
+
+model_one = Model.create(organization: organization,
+                         name: 'Series 1',
+                         model_slug: 'serie_1')
+
+model_two = Model.create(organization: organization,
+                         name: 'Series 2',
+                         model_slug: 'serie_2')
+
+ModelType.create(name: 'bmw_125i',
+                 model_type_slug: '125i',
+                 model_type_code: 'BMW',
+                 base_price: 1000,
+                 model: model_one)
+
+ModelType.create(name: 'bmw_330i',
+                 model_type_slug: '330i',
+                 model_type_code: 'BMW',
+                 base_price: 5000,
+                 model: model_two)
